0c13fe0ca7d8e3f5f7691cddcd058903571cc6e7
Tan Wei Yang <yangah9991@gmail.com>
2020-01-14 10:04:54 +0800

    Added color

--- Sandbox/src/Color.h
+++ Sandbox/src/Color.h
@@ -4,8 +4,13 @@
 
 namespace Color {
 
-	static glm::vec4 Black = { 0.0f, 0.0f, 0.0f, 1.0f };
-	static glm::vec4 White = { 1.0f, 1.0f, 1.0f, 1.0f };
-	static glm::vec4 Red = { 1.0f, 0.0f, 0.0f, 1.0f };
+	static glm::vec4 Black  = { 0.0f, 0.0f, 0.0f, 1.0f };
+	static glm::vec4 White  = { 1.0f, 1.0f, 1.0f, 1.0f };
+	static glm::vec4 Red    = { 1.0f, 0.0f, 0.0f, 1.0f };
+	static glm::vec4 Green  = { 0.0f, 1.0f, 0.0f, 1.0f };
+	static glm::vec4 Blue   = { 0.0f, 0.0f, 1.0f, 1.0f };
+	static glm::vec4 Cyan   = { 0.0f, 1.0f, 1.0f, 1.0f };
+	static glm::vec4 Purple = { 1.0f, 0.0f, 1.0f, 1.0f };
+	static glm::vec4 Yellow = { 1.0f, 1.0f, 0.0f, 1.0f };
 
 }
